cf45c09eee9300144ab66c39874d76ae2ea91314
jeanmariemcc <84250957+jeanmariemcc@users.noreply.github.com>
2021-11-22 16:02:12 -0500

    updates to css and routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,33 @@ import "./App.css";
 // import "./index.css";
 import Main from "./Components/Main";
 import Aside from "./Components/Aside";
+import Create from "./Components/Create";
+import Register from "./Components/Register";
+import Login from "./Components/Login";
+import Logout from "./Components/Logout";
+import AllPosts from "./Components/AllPosts";
+
+import { BrowserRouter } from "react-router-dom"; //don't need this because it is in index.js
+import { Route, Link } from "react-router-dom";
+import { Routes } from "react-router-dom";
 
 function App() {
 	return (
 		<div className="App Container">
 			<Aside />
-			<Main />
+			<Routes>
+				<Route path="/" exact element={<Main />} />
+				<Route path="/create" element={<Create />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/logout" element={<Logout />} />
+				<Route path="/allposts" element={<AllPosts />} />
+				{/* <Route */}
+				{/* path="/profile" */}
+				{/* element={<Profile email="test@test.com" numPosts="4" />} */}
+				/>
+				{/* <Route path="*" exact element={<Error />} /> */}
+			</Routes>
 		</div>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 // Routes
 import Nav from "./Components/Nav";
 import Footer from "./Components/Footer";
 
-// import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Nav />
-		<App />
-		<Footer />
+		<BrowserRouter>
+			<Nav />
+			<App />
+			<Footer />
+		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
