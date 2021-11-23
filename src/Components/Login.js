7f3e79d1a387408e97c8e159ef5ebf5538a52130
Randy McMillen <84250923+RandyMac22@users.noreply.github.com>
2021-11-22 19:26:25 -0600

    added forms to register and login pages and updated css

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -6,8 +6,22 @@ class Login extends React.Component {
 		// console.log(links);
 		return (
 			<main className="Main">
-				<h1>Login Route</h1>
-				{/* <Posts /> */}
+				<h1>Login</h1>
+				<form>
+                    <div className="form-control">
+                        <label for="email">Email</label>
+                        <input name="email" type="text"/>
+                    </div>
+                    
+                    <div className="form-control">
+                        <label for="password">Password</label>
+                        <input name="password" type="password"/>
+                    </div>
+                   
+                    <div className="form-control">
+                        <button type="submit">Register</button>
+                    </div>
+                </form>
 			</main>
 		);
 	}
