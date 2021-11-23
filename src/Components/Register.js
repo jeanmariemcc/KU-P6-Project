1ccbc1b794f66e505b2689dade7cc9df909e50d5
Randy McMillen <84250923+RandyMac22@users.noreply.github.com>
2021-11-22 19:18:08 -0600

    update footer and working on register page
    updated the footer and working on register page

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -6,8 +6,27 @@ class Register extends React.Component {
 		// console.log(links);
 		return (
 			<main className="Main">
-				<h1>Register Route</h1>
-				{/* <Posts /> */}
+				<h1>Register</h1>
+				<form>
+                    <div className="form-control">
+                        <label for="email">Email</label>
+                        <input name="email" type="text"/>
+                    </div>
+                    
+                    <div className="form-control">
+                        <label for="password">Password</label>
+                        <input name="password" type="password"/>
+                    </div>
+                   
+                    <div className="form-control">
+                        <label for="rePassword">Repeat Password</label>
+                        <input name="rePassword" type="password"/>
+                    </div>
+                   
+                    <div className="form-control">
+                        <button type="submit">Register</button>
+                    </div>
+                </form>
 			</main>
 		);
 	}
