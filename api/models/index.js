ef9a96f9489e857ad0a7f4f50ae6af31ba14e946
Randy McMillen <84250923+RandyMac22@users.noreply.github.com>
2021-11-27 13:53:25 -0600

    Update index.js

--- api/models/index.js
+++ api/models/index.js
@@ -1,9 +1,7 @@
 const User = require('./User');
 const Articles = require('./Articles');
-const TokenBlacklist = require('./TokenBlacklist');
 
 module.exports = {
     User,
-    Articles,
-    TokenBlacklist
+    Articles
 };
