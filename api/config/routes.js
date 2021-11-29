ea06b9617cae5dd417cca27c0b3e28cece88ddc4
Randy McMillen <84250923+RandyMac22@users.noreply.github.com>
2021-11-28 21:31:04 -0600

    latest update

--- api/config/routes.js
+++ api/config/routes.js
@@ -3,9 +3,9 @@ const router = require("../routes/");
 module.exports = (app) => {
 	app.use("/atlas/users", router.users);
 
-	app.use("/atlas/articles", router.articles);
+    app.use('/api/users', router.users);
 
-	app.use("*", (req, res, next) =>
-		res.send("<h1> Something went wrong. Try again. :thumbsup: </h1>")
-	);
+    app.use('/api/articles', router.articles);
+
+    app.use('*', (req, res, next) => res.send('<h1> Something went wrong. Try again. :thumbsup: </h1>'))
 };
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,7 @@
 const controllers = require("../controllers");
 const router = require("express").Router();
 
-router.get("/", controllers.users.get);
+router.get('/', controllers.users.get);
 
 router.post("/register", controllers.users.post.register);
 
