54b123949e804cb52056e14ec42f3be18edadc7b
jeanmariemcc <84250957+jeanmariemcc@users.noreply.github.com>
2021-11-28 20:55:29 -0500

    allPosts = Main
    allPosts routes to Main
    removed allPosts from App.js

--- src/App.js
+++ src/App.js
@@ -6,14 +6,13 @@ import Create from "./Components/Create";
 import Register from "./Components/Register";
 import Login from "./Components/Login";
 import Logout from "./Components/Logout";
-import AllPosts from "./Components/AllPosts";
+// import AllPosts from "./Components/AllPosts";
 
 // import { BrowserRouter } from "react-router-dom"; //don't need this because it is in index.js
 import { Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
 
 function App(props) {
-
 	return (
 		<div className="App Container">
 			<Aside />
@@ -21,11 +20,9 @@ function App(props) {
 				<Route path="/" exact element={<Main />} />
 				<Route path="/create" element={<Create />} />
 				<Route path="/register" element={<Register />} />
-				<Route 
-					path="/login" 
-					element={<Login />} />
+				<Route path="/login" element={<Login />} />
 				<Route path="/logout" element={<Logout />} />
-				<Route path="/allposts" element={<AllPosts />} />
+				<Route path="/allposts" exact element={<Main />} />
 				{/* <Route */}
 				{/* path="/profile" */}
 				{/* element={<Profile email="test@test.com" numPosts="4" />} */}
---
+++ src/Components/AllPosts-oldjm.js
@@ -0,0 +1,36 @@
+import React from "react";
+import posts from "../Data/posts.json";
+import OnePost from "./OnePost";
+
+class AllPosts extends React.Component {
+	render() {
+		let limit;
+		if (this.props.limit !== undefined) {
+			limit = Number(this.props.limit);
+		}
+		console.log(limit);
+		return (
+			<main className="Main">
+				<h1>All Posts Route</h1>
+				{/* <Posts /> */}
+				<div className="Posts">
+					{posts.map((post, index) => {
+						// if a limit was passed, check to see if we have passed the limit , this comes from the input route
+						if (!limit || index < limit) {
+							return (
+								<OnePost
+									text={post.text}
+									title={post.title}
+									image={post.image}
+								/>
+							);
+						}
+					})}
+				</div>
+				); 
+			</main>
+		);
+	}
+}
+
+export default AllPosts;
